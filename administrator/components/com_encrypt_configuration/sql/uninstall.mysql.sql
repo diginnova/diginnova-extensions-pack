12584a1e67564e667ad984564deeaf70a12c942c
diginnova <laqu.shas@gmail.com>
2015-11-02 20:51:54 +0100

    alpha

--- administrator/components/com_encrypt_configuration/sql/uninstall.mysql.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS `#__encrypt_keys`;
-DROP TABLE IF EXISTS `#__encrypt_controls`;
-DROP TABLE IF EXISTS `#__encrypt_gendata`;
-DROP TABLE IF EXISTS `#__encrypt_access_lock`;
-
-
-
--- logs/error.php
+++
@@ -1,7 +0,0 @@
-#
-#<?php die('Forbidden.'); ?>
-#Date: 2015-11-02 16:15:34 UTC
-#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
-
-#Fields: datetime	priority clientip	category	message
-2015-11-02T16:15:34+00:00	INFO 95.233.53.110	joomlafailure	Username e Password non coincidono o non hai ancora un account
